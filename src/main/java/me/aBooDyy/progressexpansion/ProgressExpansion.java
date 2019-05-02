31f5acf23004d9ca88c188d83c35b8f6ec720622
aBo0oDyy <35378106+aBo0oDyy@users.noreply.github.com>
2019-05-03 00:35:30 +0300

    New version 1.3

--- src/main/java/me/aBooDyy/progressexpansion/ProgressExpansion.java
+++ src/main/java/me/aBooDyy/progressexpansion/ProgressExpansion.java
@@ -27,7 +27,7 @@ public class ProgressExpansion extends PlaceholderExpansion implements Configura
 
     @Override
     public String getVersion() {
-        return "1.2";
+        return "1.3";
     }
 
     @Override
