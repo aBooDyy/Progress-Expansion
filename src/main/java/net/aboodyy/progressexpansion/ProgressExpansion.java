d089295e3a113a7f9010231dc033b3b3311de381
aBo0oDyy <35378106+aBo0oDyy@users.noreply.github.com>
2020-04-05 00:16:46 +0300

    v1.7 and copyright notice

--- src/main/java/net/aboodyy/progressexpansion/ProgressExpansion.java
+++ src/main/java/net/aboodyy/progressexpansion/ProgressExpansion.java
@@ -1,3 +1,23 @@
+/*
+
+    Progress Expansion - A PlaceholderAPI expansion that allows you to create a progress bar placeholder based on your inputs
+    Copyright (C) 2019-2020 aBooDyy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 package net.aboodyy.progressexpansion;
 
 import me.clip.placeholderapi.PlaceholderAPI;
@@ -25,7 +45,7 @@ public class ProgressExpansion extends PlaceholderExpansion implements Configura
 
     @Override
     public String getVersion() {
-        return "1.6";
+        return "1.7";
     }
 
     @Override
